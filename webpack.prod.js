af1c92b559c2240e12e61e5882fa5e926bedc083
Ewoskyx <ewoskyx@gmail.com>
2021-12-20 21:09:56 +0300

    Initial project setup

---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const config = require('./webpack.config.js');
+
+module.exports = merge(config, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
