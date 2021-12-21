23894301d384dca10ee5fff9b1b2b520e0898f79
Ewoskyx <ewoskyx@gmail.com>
2021-12-21 17:24:39 +0300

    Add API +  cards filled by API to  home page

--- src/app/api.js
+++ src/app/api.js
@@ -1,1 +1,11 @@
-// API call here //
+// Base API call  //
+const apiKey = 'api_key=00d82371d477e875486a05abe6476c67';
+const apiUrl = `https://api.themoviedb.org/3/movie/popular?${apiKey}`;
+
+async function getData() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const res = await data.results;
+  return res;
+}
+export default getData;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import './main.scss';
 import 'bootstrap';
+import getData from './app/api.js';
+
+const movieList = document.getElementById('movie-list');
+
+const renderMovies = () => {
+  getData().then((movies) => {
+    movies.forEach((movie) => {
+      movieList.innerHTML += `
+<div class="card p-0" style="width: 18rem;">
+  <img class="card-img-top" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Card image cap">
+  <div class="card-body">
+  <div class="h-25 w-100">
+    <h5 class="h6">${movie.title}</h5>
+  </div>
+  <div class="h-25 w-100 d-flex flex-column align-items-end"> 
+    <a class="btn-like"></a>
+    <span>5 Likes</span>
+  </div>  
+  <div class="h-50 d-flex flex-column gap-2">
+    <a href="#" class="btn btn-dark">Comments</a>
+    <a href="#" class="btn btn-warning">Rezervations</a>
+  </div>  
+  </div>
+</div> `;
+    });
+  });
+};
+renderMovies();
