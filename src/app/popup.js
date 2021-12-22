fc15d541d8338ffed8f9028e57f4b39980b7b095
MeqdamAlqudah <memuq56@gmail.com>
2021-12-22 14:22:16 +0200

    return Evrean changes

---
+++ src/app/popup.js
@@ -0,0 +1,40 @@
+const displayPopup = (element) => ` <div class="modal-content w-100 align-items-center border border-dark border-3">
+<div class="modal-header w-75 border-0">
+  <button type="button" class="btn-close" onclick = "close()"
+  data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body">
+<div class="img-container d-flex justify-content-center flex-column align-items-center">
+<h5 class="modal-title">${element.original_title}</h5>
+<img src="https://image.tmdb.org/t/p/w500${element.poster_path}" class="w-50"></div>
+  <p>Movie Recap:-</p>
+  <p class="recap">${element.overview}</p>
+ <div class="container">
+     <ul class="row">
+    <li id="popularity" class="col d-flex">Popularity: <p class="ms-2">${element.popularity}</p> </li>
+    <li id="language" class="col d-flex"> Language: <p class="ms-2">${element.original_language}</p> </li>
+</ul>
+<ul class="row">
+    <li id="release-date" class="col d-flex">
+     Release: <p class="ms-2">${element.release_date}</p></li>
+    <li id="vote" class="col d-flex"> Vote:<p class="ms-2">${element.vote_count}</p></li>
+</ul>
+ </div>
+</div>
+<h6>Comments (2)</h6>
+<ul class="comments-list">
+
+</ul>
+<h6>Add a comment</h6>
+<div class="input-group d-flex flex-column flex-nowrap  w-50 addcomment">
+    <input type="text" placeholder="Your name" class="Name
+     form-control w-75 mb-3 border border-dark border-3">
+    <input type="text" class="insights form-control w-100
+    mb-3 border border-dark border-3">
+    <button type="button" class="comment btn
+    btn-outline-secondary w-75 align-self-start
+    mb-3 border border-dark border-3">Comment</button>
+  </div>
+</div>`;
+
+export default displayPopup;
--- src/index.js
+++ src/index.js
@@ -1,122 +1,75 @@
 import './main.scss';
 import 'bootstrap';
 import getData from './app/api.js';
+import displayPopup from './app/popup';
 
 const movieList = document.getElementById('movie-list');
-const commentsArray = JSON.parse(localStorage.getItem('commentsArray') || '[]');
-const displayComments = (el) => {
-  const ul = document.querySelector('.comments-list');
-  const li = document.createElement('li');
-
-  li.textContent = `${el.date} ${el.name} ${el.insight}`;
-  ul.appendChild(li);
-};
-
-const displayPopup = (element) => {
-  const modal = document.querySelector('.modal');
-  modal.style.display = 'block';
-  const img = document.createElement('img');
-  img.setAttribute('src', `https://image.tmdb.org/t/p/w500${element.poster_path}`);
-  img.classList.add('w-50');
-  const title = element.original_title;
-  const h = document.querySelector('.img-container h5');
-  h.textContent = title;
-  const recap = document.querySelector('.recap');
-  recap.textContent = element.overview;
-  const popularity = document.querySelector('#popularity p');
-  const language = document.querySelector('#language p');
-  const release = document.querySelector('#release-date p');
-  const vote = document.querySelector('#vote p');
-  popularity.textContent = element.popularity;
-  language.textContent = element.original_language;
-  release.textContent = element.release_date;
-  vote.textContent = element.vote_count;
-  const container = document.querySelector('.img-container');
-  container.appendChild(img);
-  modal.style.backgroundColor = '#0000008a';
-  modal.style.backdropFilter = 'blur(3px)';
-  const comment = document.querySelector('.comment');
-  comment.addEventListener('click', () => {
-    const name = document.querySelector('.Name').value;
-    const insight = document.querySelector('.insights').value;
-    const d = new Date();
-    const date = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
-    if (name && insight) {
-      const el = { date, name, insight };
-      commentsArray.push(el);
-      localStorage.setItem('commentsArray', JSON.stringify(commentsArray));
-      displayComments(el);
-    }
-  });
-  const close = document.querySelector('.btn-close');
-  close.addEventListener('click', () => {
-    modal.style.display = 'none';
-    if (container.contains(img)) {
-      container.removeChild(img);
-    }
-  });
-  commentsArray.forEach((element) => {
-    displayComments(element);
-  });
-};
 
 const renderMovies = () => {
   getData().then((movies) => {
-    for (let i = 0; i < movies.length; i += 1) {
-      const div1 = document.createElement('div');
-      div1.classList.add('card');
-      div1.classList.add('p-0');
-      div1.style.width = '18rem';
-      const div2 = document.createElement('div');
-      div2.classList.add('card-body');
-      const div3 = document.createElement('div');
-      div3.classList.add('h-25');
-      div3.classList.add('w-100');
-      const div4 = document.createElement('div');
-      div4.classList.add('h-25');
-      div4.classList.add('w-100');
-      div4.classList.add('d-flex');
-      div4.classList.add('flex-column');
-      div4.classList.add('align-items-end');
-      const div5 = document.createElement('div');
-      div5.classList.add('h-50');
-      div5.classList.add('d-flex');
-      div5.classList.add('flex-column');
-      div5.classList.add('gap-2');
-      const img = document.createElement('img');
-      img.setAttribute('src', `https://image.tmdb.org/t/p/w500${movies[i].poster_path}`);
-      img.setAttribute('alt', 'Card image cap');
-      const h5 = document.createElement('h5');
-      h5.classList.add('h6');
-      const a1 = document.createElement('a');
-      a1.classList.add('btn-like');
-      const a2 = document.createElement('a');
-      a2.classList.add('display-comments');
-      a2.classList.add('btn');
-      a2.classList.add('btn-dark');
-      const a3 = document.createElement('a');
-      const span = document.createElement('span');
-      span.innerText = '5 Likes';
-      a3.classList.add('btn');
-      a3.classList.add('btn-warning');
-      h5.innerText = movies[i].title;
-      div1.appendChild(img);
-      div1.appendChild(div2);
-      div2.appendChild(div3);
-      div3.appendChild(h5);
-      a2.innerText = 'Comments';
-      a3.innerText = 'Reservations';
-      div4.appendChild(a1);
-      div4.appendChild(span);
-      div5.appendChild(a2);
-      div5.appendChild(a3);
-      div2.appendChild(div4);
-      div2.appendChild(div5);
-      movieList.appendChild(div1);
-      a2.addEventListener('click', () => {
-        displayPopup(movies[i]);
-      });
-    }
+    movies.forEach((movie) => {
+      movieList.innerHTML += `
+<div class="card p-0" style="width: 18rem;">
+  <img class="card-img-top" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Card image cap">
+  <div class="card-body">
+  <div class="h-25 w-100">
+    <h5 class="h6">${movie.title}</h5>
+  </div>
+  <div class="h-25 w-100 d-flex flex-column align-items-end"> 
+    <a class="btn-like"></a>
+    <span>5 Likes</span>
+  </div>  
+  <div class="h-50 d-flex flex-column gap-2">
+    <a href="#" class="btn btn-dark">Comments</a>
+    <a href="#" class="btn btn-warning">Rezervations</a>
+  </div>  
+  </div>
+</div> `;
+    });
   });
 };
 renderMovies();
+
+function display() {
+  getData().then((movies) => {
+    movies.forEach((movie, index) => {
+      const commentButton = document.querySelectorAll('.btn.btn-dark')[index];
+      const modal = document.querySelector('.modal-dialog');
+      commentButton.addEventListener('click', async () => {
+        modal.innerHTML += displayPopup(movie);
+        document.querySelector('.modal').style.display = 'block';
+        const close = document.querySelector('.btn-close');
+        const comment = document.querySelector('.comment');
+        close.addEventListener('click', () => {
+          document.querySelector('.modal').style.display = 'none';
+          modal.innerHTML = '';
+        });
+        const commentsArray = JSON.parse(localStorage.getItem('commentsArray') || '[]');
+        const displayComments = (el) => {
+          const ul = document.querySelector('.comments-list');
+          const li = document.createElement('li');
+
+          li.textContent = `${el.date} ${el.name} ${el.insight}`;
+          ul.appendChild(li);
+        };
+        comment.addEventListener('click', () => {
+          const name = document.querySelector('.Name').value;
+          const insight = document.querySelector('.insights').value;
+          const d = new Date();
+          const date = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+          if (name && insight) {
+            const el = { date, name, insight };
+            commentsArray.push(el);
+            localStorage.setItem('commentsArray', JSON.stringify(commentsArray));
+            displayComments(el);
+          }
+        });
+        commentsArray.forEach((element) => {
+          displayComments(element);
+        });
+      });
+    });
+  });
+}
+
+setTimeout(display, 2000);
