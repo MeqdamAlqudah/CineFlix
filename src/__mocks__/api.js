bc311d58cfa3e5cb510e0f8ef2cab134205d6299
MeqdamAlqudah <memuq56@gmail.com>
2021-12-24 01:24:56 +0200

    Delete comments

--- src/__mocks__/api.js
+++ src/__mocks__/api.js
@@ -1,17 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
-// const sendCommentsToApi = ({
-//   type, body,
-// }) => {
-//   const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/rEViJgSZrOmcE6w0blo4/comments';
-//   fetch(url, {
-//     method: type,
-//     body,
-//     headers: {
-//       'Content-type': 'application/json; charset=utf-8',
-//     },
-//   });
-// };
 
 const reciveCommentsApi = (id) => {
   const comments = Promise.resolve([{ username: '', creation_date: '2021-12-23', comment: '' }, { username: 'Meqdam', comment: 'great movie', creation_date: '2021-12-23' }]);
