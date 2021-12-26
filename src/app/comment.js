981eeff599bddfc7250c8fa7472326490fe87733
MeqdamAlqudah <memuq56@gmail.com>
2021-12-26 23:50:45 +0200

    fix the error

--- src/app/comment.js
+++ src/app/comment.js
@@ -14,17 +14,17 @@ const displayCommentData = (movie, reciveCommentsApi, sendCommentsToApi, counter
     if (!username) {
       e.preventDefault();
       errorElement.innerText = 'Please enter your name and comment';
-    } else if (username.length > 10) {
+    } else if (username.length > 12) {
       e.preventDefault();
-      errorElement.innerText = 'you just allowed to give 40 word length name';
-    } else if (comment.length > 30) {
+      errorElement.innerText = 'you just allowed to give 12 word length name';
+    } else if (comment.length > 40) {
       e.preventDefault();
-      errorElement.innerText = 'you just allowed to give 99 word length comment';
+      errorElement.innerText = 'you just allowed to give 40 word length comment';
     } else {
       errorElement.innerText = '';
       const d = new Date();
       // eslint-disable-next-line camelcase
-      const creation_date = `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`;
+      const creation_date = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
 
       // Validate-contact-form function
       if (username && comment) {
